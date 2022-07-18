4e1a90acd6f2b0c4a59c3a5f235938bbc48c322d
Parazzels <o-kochergin@bk.ru>
2022-07-18 11:48:26 +0300

    Добавьте файлы проекта.

---
+++ Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            while (num > 0)
+            {
+                Console.Write(num % 10);
+                num /= 10;
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
